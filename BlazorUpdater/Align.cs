9ce61dfce05b182512fb2b5f2b960d606dd25aeb
Szöllősi Dániel <szollosi.daniel0@gmail.com>
2023-11-25 17:52:28 +0100

    Added NET 8 support

--- BlazorUpdater/Align.cs
+++ BlazorUpdater/Align.cs
@@ -1,8 +1,16 @@
 namespace BlazorUpdater;
 
+/// <summary>
+/// Update bar position
+/// </summary>
 public enum Align
 {
-    None = 0,
+    /// <summary>
+    /// Update bar will be positioned to the top of the screen
+    /// </summary>
     Top,
+    /// <summary>
+    /// Update bar will be positioned to the bottom of the screen
+    /// </summary>
     Buttom
 }
